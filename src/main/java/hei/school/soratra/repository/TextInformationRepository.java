0e5db70643bd7e2f9185c9f8b80b894d282e41bf
MiiJack <f.rama723@gmail.com>
2024-03-25 09:59:17 +0300

    fix: fixed controller

--- src/main/java/hei/school/soratra/repository/TextInformationRepository.java
+++ src/main/java/hei/school/soratra/repository/TextInformationRepository.java
@@ -2,5 +2,7 @@ package hei.school.soratra.repository;
 
 import hei.school.soratra.repository.model.TextInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TextInformationRepository extends JpaRepository<TextInformation, String> {}
