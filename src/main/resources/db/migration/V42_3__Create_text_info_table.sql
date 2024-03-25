67aa266d45cfb94d037a22d9a6db832831984016
MiiJack <f.rama723@gmail.com>
2024-03-25 08:38:41 +0300

    feat: migration script of text info table

---
+++ src/main/resources/db/migration/V42_3__Create_text_info_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE text_information (
+    id VARCHAR(255) PRIMARY KEY,
+    text_bucket_key VARCHAR(255),
+    original_bucket_key VARCHAR(255),
+    creation_datetime TIMESTAMP WITH TIME ZONE  DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    update_datetime TIMESTAMP WITH TIME ZONE  DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
